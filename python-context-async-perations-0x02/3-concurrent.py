7d19fd2288cad37dc25935967c86aa54b6dae69f
AustinMusuya <amusuya57@gmail.com>
2025-07-11 15:35:44 +0300

    refactor: create async DB queries to use manual connection and cursor handling with aiosqlite and fetch all user and all users older than 40 repsectively.

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,54 @@
+"""
+Objective: Run multiple database queries concurrently using asyncio.gather.
+
+Instructions:
+Use the aiosqlite library to interact with SQLite asynchronously. 
+To learn more about it, click here.
+
+Write two asynchronous functions: 
+async_fetch_users() and async_fetch_older_users() 
+that fetches all users and users older than 40 respectively.
+
+Use the asyncio.gather() to execute both queries concurrently.
+
+Use asyncio.run(fetch_concurrently()) to run the concurrent fetch
+
+"""
+
+import asyncio
+import aiosqlite
+
+
+async def async_fetch_users():  # async function to fetch all users from sqlite db
+    db = await aiosqlite.connect("users.db")
+    cursor = await db.execute('SELECT * FROM users')
+    rows = await cursor.fetchall()
+    print("\n[All Users]")
+    for row in rows:
+        print(row)
+    # close connections
+    await cursor.close()
+    await db.close()
+
+
+async def async_fetch_older_users():  # async function to fetch all users older than 40 from sqlite db
+    db = await aiosqlite.connect("users.db")
+    query = 'SELECT * FROM users WHERE age > ?'
+    val = (40,)
+    cursor = await db.execute(query, val)
+    rows = await cursor.fetchall()
+    print("\n[All Users above 40 years]")
+    for row in rows:
+        print(row)
+    # Close connections
+    await cursor.close()
+    await db.close()
+
+
+async def fetch_concurrently():
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users(),
+    )
+
+asyncio.run(fetch_concurrently())
