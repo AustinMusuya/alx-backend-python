2bcdd4c294e0d1ab9da96f68c708101b48fd0ac2
AustinMusuya <amusuya57@gmail.com>
2025-07-18 16:19:50 +0300

    feat(viewsets): implement Message and Conversation viewsets with DRF ModelViewSet
    - Added IsAuthenticated permission to protect endpoints
    - Simplified CRUD operations using DRF ViewSet architecture
    - Aligned with project checker requirements for nested resources

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from .serializers import ConversationSerializer, MessageSerializer
+from .models import Message, Conversation
+from rest_framework import generics, viewsets
 
-# Create your views here.
+# authentication & Permisions api views
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+# viewset to perform CRUD operations
+
+class MessageViewSet(viewsets.ModelViewSet):
+    # authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    
+    def get(self, request):
+        return Response({'message': 'Hello authenticated user'})
+
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    # authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    
+    def get(self, request):
+        return Response({'message': 'Hello authenticated user'})
