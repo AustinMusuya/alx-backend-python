5612d691d80d10d2c33952d9a6d2b92d0bb99088
AustinMusuya <amusuya57@gmail.com>
2025-07-18 16:41:31 +0300

    feat(api): implement Conversation and Message viewsets with permissions and filters

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,32 +1,35 @@
 from django.shortcuts import render
 from .serializers import ConversationSerializer, MessageSerializer
 from .models import Message, Conversation
-from rest_framework import generics, viewsets
+from rest_framework import viewsets, status, filters
 
 # authentication & Permisions api views
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
 # viewset to perform CRUD operations
 
-class MessageViewSet(viewsets.ModelViewSet):
-    # authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
-    
-    def get(self, request):
-        return Response({'message': 'Hello authenticated user'})
-
-
 class ConversationViewSet(viewsets.ModelViewSet):
-    # authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     
     def get(self, request):
-        return Response({'message': 'Hello authenticated user'})
+        return Response({'message': 'Hello authenticated user'}, status=status.HTTP_200_OK)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = MessageSerializer
+    filter_backends = [filters.OrderingFilter]  
+    ordering_fields = ['sent_at'] 
+
+    def get_queryset(self):
+        conversation_id = self.request.query_params.get('conversation_id')
+        if conversation_id:
+            return Message.objects.filter(conversation_id=conversation_id)
+        return Message.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
