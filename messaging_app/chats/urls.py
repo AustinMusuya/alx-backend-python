a46f61a0eac07ef93caf5f7a178740b1fc873212
AustinMusuya <amusuya57@gmail.com>
2025-07-18 16:42:08 +0300

    feat(api): register conversation and message viewsets with DRF router

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from .views import ConversationViewSet, MessageViewSet
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
