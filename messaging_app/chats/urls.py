678fb85f982d5639a30b1158336ea0909ee3a0a2
AustinMusuya <amusuya57@gmail.com>
2025-07-25 11:37:40 +0300

    feat: add auth urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,6 +2,11 @@ from django.urls import path, include
 from .views import ConversationViewSet, MessageViewSet
 from rest_framework_nested.routers import NestedDefaultRouter 
 from rest_framework.routers import DefaultRouter
+from .auth import (
+    RegisterUserAPIView,
+    LoginUserAPIView,
+    LogoutUserAPIView
+)
 
 
 router = DefaultRouter()
@@ -12,5 +17,8 @@ nested_router.register(r'messages', MessageViewSet, basename='conversation-messa
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(nested_router.urls)), 
+    path('', include(nested_router.urls)),
+    path('auth/register/', RegisterUserAPIView.as_view(), name='register'),
+    path('auth/login/', LoginUserAPIView.as_view(), name='login'),
+    path('auth/logout/', LogoutUserAPIView.as_view(), name='logout'),
 ]
