dfc724120ea72b4793cdbf170710283e46563fcc
AustinMusuya <amusuya57@gmail.com>
2025-07-31 11:36:24 +0300

    feat(signals): add post_save and pre_save handlers for notifications and message edits
    - Automatically create notifications when a new message is sent
    - Log previous message content into MessageHistory before edits

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,24 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from .models import Message, Notification, MessageHistory
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(message=instance, user=instance.receiver)
+
+
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if not instance._state.adding:  # This means it's being updated, not created
+        try:
+            old_instance = Message.objects.get(pk=instance.pk)
+            if old_instance.content != instance.content:
+                MessageHistory.objects.create(
+                    message=old_instance,
+                    old_content=old_instance.content
+                )
+                instance.edited = True  # Mark message as edited
+        except Message.DoesNotExist:
+            pass  # This might occur rarely in race conditions
