ac4d2d7fd13634c92501b4b0d09d3619561699be
AustinMusuya <amusuya57@gmail.com>
2025-07-31 11:42:37 +0300

    feat: add field 'edited_by' to track which user edited a message

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -31,6 +31,7 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
     old_content = models.TextField()
     edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return f"History for {self.message.message_id} at {self.edited_at}"
